713c2394ab7945fb9a0aa196abef70975a32f4de
Atul-Acharya-17 <adiatul2001@gmail.com>
2022-03-15 01:08:27 +0800

    Refactored and Copied Hash and Nested Loop

--- SimpleDBEngine/src/simpledb/materialize/MinFn.java
+++ SimpleDBEngine/src/simpledb/materialize/MinFn.java
@@ -25,7 +25,6 @@ public class MinFn implements AggregationFn {
     */
    public void processFirst(Scan s) {
       val = s.getVal(fldname);
-      System.out.println("Val " + val.asDouble());
    }
    
    /**
